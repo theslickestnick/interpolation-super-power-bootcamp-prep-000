b9f9c89d74fa996183e6230dbba3bf71aa31b7b6
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-07 02:33:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  print "#{colors[0]}"
+  print "#{colors[1]}"
+  print "#{colors[2]}"
+  print "#{colors[3]}"
+  print "#{colors[4]}"
+  print "#{colors[5]}"
+  print "#{colors[6]}"
+end
